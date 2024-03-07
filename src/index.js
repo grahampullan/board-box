811075d4801f142a57c9b0bd463a9de40c2c3f6a
Graham Pullan <gp10006@cam.ac.uk>
2024-03-07 13:57:36 +0000

    Component class added, examples updated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 export { Board } from './Board.js';
 export { Box } from './Box.js';
 export { Context } from './Context.js';
+export { Component } from './Component.js';
+export { Observable } from './Observable.js';
