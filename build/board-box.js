00b2b70a26f1fde07ad7c34541007d85e8861e97
Graham Pullan <gp10006@cam.ac.uk>
2024-10-30 17:23:26 +0000

    build

--- build/board-box.js
+++ build/board-box.js
@@ -4218,6 +4218,15 @@ class Observable{
 
     }
 
+    subscribeWithData(observerAndData) {
+
+        let id = this.observerIdMax;
+        this.observers.push({observer:observerAndData.observer, data:observerAndData.data ,id});
+        this.observerIdMax++;
+        return id
+
+    }
+
     isSubscribed(observer) {
 
         return this.observers.map( d => d.observer).includes(observer);
